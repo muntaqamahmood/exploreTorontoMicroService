5b5e2e6ca0043e00862654e49ef91c8143ae3dbc
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2023-06-21 23:49:07 -0400

    init: Adding files

---
+++ exploretoronto/src/main/java/com/example/ec/exploretoronto/ExploretorontoApplication.java
@@ -0,0 +1,14 @@
+package com.example.ec.exploretoronto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//this is where our microservice starts with the main function and
+public class ExploretorontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploretorontoApplication.class, args);
+	}
+
+}
