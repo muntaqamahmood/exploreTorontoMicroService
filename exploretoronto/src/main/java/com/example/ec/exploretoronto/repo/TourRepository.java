0b12c8a87d3a69e615114c4300ae36aef65e9fa4
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2023-06-25 18:23:54 -0400

    feat: paging and sorting using PagingAndSorting Repository

--- exploretoronto/src/main/java/com/example/ec/exploretoronto/repo/TourRepository.java
+++ exploretoronto/src/main/java/com/example/ec/exploretoronto/repo/TourRepository.java
@@ -1,12 +1,15 @@
 package com.example.ec.exploretoronto.repo;
 
 import com.example.ec.exploretoronto.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
     // list of Tours the Tour Package is in
-    List<Tour> findByTourPackageCode(@Param("code") String tourPackage_code);
+    Page<Tour> findByTourPackageCode(@Param("code") String tourPackage_code, Pageable pageable);
 }
