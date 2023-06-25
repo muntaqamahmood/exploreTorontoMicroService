8d4325044be8af2ff87a19e3f1c398de3605ba85
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2023-06-25 17:43:57 -0400

    feat: added new spring data JPA crud method for tours

--- exploretoronto/src/main/java/com/example/ec/exploretoronto/repo/TourRepository.java
+++ exploretoronto/src/main/java/com/example/ec/exploretoronto/repo/TourRepository.java
@@ -2,7 +2,11 @@ package com.example.ec.exploretoronto.repo;
 
 import com.example.ec.exploretoronto.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
-
+    // list of Tours the Tour Package is in
+    List<Tour> findByTourPackageCode(@Param("code") String tourPackage_code);
 }
